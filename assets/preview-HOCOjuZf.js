34030032c5171d6cb92f034edf27c7eed59af3a2
gilavi <gilavi@users.noreply.github.com>
2026-06-18 21:48:27 +0000

    deploy: gilavi/Sarke2.0@b2a1ddde1cacc31d5f0d434e8077998222907f33

--- assets/preview-HOCOjuZf.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./entry-preview-CBfWjze5.js";import{e}from"./entry-preview-docs-DtJq7fMQ.js";import{L as t}from"./iframe-D1qZMWTD.js";import"./chunk-XP5HYGXS-BHXC9YeQ.js";import"./index-CK_08F47.js";import"./index-LKo5rBYE.js";function d(r){return t({...r,addons:[o,e,...r.addons??[]]})}export{d as __definePreview};
