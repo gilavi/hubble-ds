6e3668bbcb5b1fbff0749535816561189027a8c6
gilavi <gilavi@users.noreply.github.com>
2026-06-19 12:57:41 +0000

    deploy: gilavi/Sarke2.0@0663f363f67ef648bf5ec5426805db75e8059be0

--- assets/preview-X42UAHMM.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./entry-preview-BBl8Pv7U.js";import{e}from"./entry-preview-docs-DtJq7fMQ.js";import{L as t}from"./iframe-B-65WHRQ.js";import"./chunk-XP5HYGXS-BHXC9YeQ.js";import"./index-CK_08F47.js";import"./index-LKo5rBYE.js";function d(r){return t({...r,addons:[o,e,...r.addons??[]]})}export{d as __definePreview};
